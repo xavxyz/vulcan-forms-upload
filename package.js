36085b87c0adf3ff7c0f63d70a2b1641a58059e2
xavcz <xavier.cazalot@gmail.com>
2016-10-03 17:09:11 +0200

    v0.27.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "xavcz:nova-forms-upload",
   summary: "Telescope Nova package extending nova:forms to upload images to Cloudinary from a drop zone.",
-  version: "0.27.1-nova",
+  version: "0.27.2-nova",
   git: 'https://github.com/xavcz/nova-forms-upload.git'
 });
 
@@ -10,8 +10,8 @@ Package.onUse( function(api) {
   api.versionsFrom("METEOR@1.0");
 
   api.use([
-    'nova:core@0.27.1-nova',
-    'nova:forms@0.27.1-nova',
+    'nova:core@0.27.2-nova',
+    'nova:forms@0.27.2-nova',
     'fourseven:scss@3.9.0'
   ]);
 
